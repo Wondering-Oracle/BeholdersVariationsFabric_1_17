9c8273671f9e7f7c2bd50ec14a997d882cfca3a0
Wondering-Oracle <73407726+Wondering-Oracle@users.noreply.github.com>
2021-06-10 23:03:27 -0400

    Initial Setup Pt.1

--- src/main/java/com/silicateseer/beholdersvariations/mixin/ExampleMixin.java
+++ src/main/java/com/silicateseer/beholdersvariations/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example.mixin;
+package com.silicateseer.beholdersvariations.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
